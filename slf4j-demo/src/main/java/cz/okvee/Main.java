6f80da115b0350d39726913a0b09afc44c8088a1
Ondrej Kvasnicka <okvasnicka@fastmail.fm>
2014-12-19 21:27:05 +0100

    marker test

--- slf4j-demo/src/main/java/cz/okvee/Main.java
+++ slf4j-demo/src/main/java/cz/okvee/Main.java
@@ -2,18 +2,20 @@ package cz.okvee;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 
 public class Main {
     public static void main(String[] args) {
         Logger log = LoggerFactory.getLogger(Main.class);
+        Marker fatal = MarkerFactory.getMarker("FATAL");
 
-        // ALL
         log.trace("Trace message");
         log.debug("Debug message");
         log.info("Info message");
         log.warn("Warn message");
         log.error("Error message");
-        // OFF
+        log.error(fatal, "Fatal message");
 
     }
 }
